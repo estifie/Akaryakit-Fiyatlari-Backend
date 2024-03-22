6a6f7944bc7cf6c2f7e6defa4741826b4adb0967
LntAxe <ertusari0@gmail.com>
2024-03-22 16:42:13 +0300

    feat: Add HTTPS support

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { config } from 'dotenv';
+import * as fs from 'fs';
 import { AppModule } from './app.module';
 config();
 
 async function bootstrap() {
+  const httpsOptions =
+    process.env.HTTPS_ENABLED === 'true'
+      ? {
+          key: fs.readFileSync(process.env.HTTPS_KEY_PATH),
+          cert: fs.readFileSync(process.env.HTTPS_CERT_PATH),
+        }
+      : null;
+
   const app = await NestFactory.create(AppModule, {
     abortOnError: false,
     logger: ['error', 'warn', 'log', 'debug'],
+    httpsOptions,
   });
 
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
 
   const version = process.env.VERSION || 'v1';
 
